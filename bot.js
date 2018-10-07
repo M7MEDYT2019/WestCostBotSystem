f4ed8b785ad6b92670d2c69a30246d870deff719
M7MEDYT2019 <42390271+M7MEDYT2019@users.noreply.github.com>
2018-10-07 15:55:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2432,7 +2432,6 @@ client.on("message", (message) => {
 });  
 
 
-const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const moment = require('moment');
